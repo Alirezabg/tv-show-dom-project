75551a9d1919f7c231a9c2e5450fefaf4f138709
Alirezabg <91377914+Alirezabg@users.noreply.github.com>
2022-02-21 07:41:14 +0000

    add episodes

--- episodes.js
+++ episodes.js
@@ -8,8 +8,8 @@ function getOneEpisode() {
     id: 4952,
     url:
       "http://www.tvmaze.com/episodes/4952/game-of-thrones-1x01-winter-is-coming",
-    name: "Winter is Coming",
-    season: 1,
+      name: "Winter is Coming",
+      season: 1,
     number: 1,
     airdate: "2011-04-17",
     airtime: "21:00",
--- script.js
+++ script.js
@@ -7,6 +7,25 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  
+  let episodesDiv = document.getElementById(`Episods`);
+
+  for (let episode of episodeList) {
+    let episodeName = document.createElement(`p`);
+    let episodeDiv = document.createElement(`div`);
+    episodeDiv.appendChild(episodeName);
+    episodesDiv.appendChild(episodeDiv);
+    episodeName.innerText = "Name : " + episode.name;
+    let episodeImage = document.createElement(`img`);
+    episodeDiv.appendChild(episodeImage);
+    episodeImage.src = episode.image.medium;
+    console.log(episode, episode.name);
+    
+  }
+  
 }
 
 window.onload = setup;
+
+
