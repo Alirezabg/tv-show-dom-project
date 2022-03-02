f74d5dfce8810448e8e55be492463c4b0a22ccae
Alirezabg <91377914+Alirezabg@users.noreply.github.com>
2022-03-02 12:10:35 +0000

    episode name

--- script.js
+++ script.js
@@ -6,26 +6,26 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 
-  
   let episodesDiv = document.getElementById(`Episods`);
 
   for (let episode of episodeList) {
-    let episodeName = document.createElement(`p`);
+    let episodeName = document.createElement(`h4`);
     let episodeDiv = document.createElement(`div`);
-    episodeDiv.appendChild(episodeName);
-    episodesDiv.appendChild(episodeDiv);
-    episodeName.innerText = "Name : " + episode.name;
     let episodeImage = document.createElement(`img`);
+    let episodeDescribe = document.createElement(`div`);
+    episodeDiv.appendChild(episodeName);
+    episodesDiv.appendChild(episodeDiv, episodeDescribe);
+    let episodeNumber =
+      (`0` + episode.season).slice(-2) + `E` + (`0` + episode.number).slice(-2);
+    episodeName.innerText = `${episode.name} - S${episodeNumber}`;
+    episodeDescribe.innerHTML = episode.summary;
+
     episodeDiv.appendChild(episodeImage);
     episodeImage.src = episode.image.medium;
+
     console.log(episode, episode.name);
-    
   }
-  
 }
 
 window.onload = setup;
-
-
