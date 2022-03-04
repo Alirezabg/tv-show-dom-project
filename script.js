d4c6b2cfd5ceaf5d7e596146dc148b3d07e03147
Alirezabg <91377914+Alirezabg@users.noreply.github.com>
2022-03-04 12:59:40 +0000

    complete level one

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 //You can edit ALL of the code here
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  fetch(`https://api.tvmaze.com/shows/82/episodes`)
+    .then((Response) => Response.json())
+    .then((data) => makePageForEpisodes(data));
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
 
   let episodesDiv = document.getElementById(`Episods`);
-
+  episodeList.forEach((element) => {
+    let selectSeries = document.getElementById(`selectSeries`);
+    let option = document.createElement(`option`);
+    option.innerText = element.name;
+    selectSeries.appendChild(option);
+  });
+  
   for (let episode of episodeList) {
     let episodeDiv = document.createElement(`div`);
     episodeDiv.className = `episodeDiv`;
@@ -25,9 +32,55 @@ function makePageForEpisodes(episodeList) {
     episodeDiv.appendChild(episodeImage);
     episodeDiv.appendChild(episodeDescribe);
 
-    episodeImage.src = episode.image.medium;
+    try {
+      episodeImage.src = episode.image.medium;
+    } catch {
+      episodeImage.src = "img/notFound.jpg";
+    }
+    let searchBar = document.getElementById(`searchBar`);
 
-    console.log(episode, episode.name);
+    searchBar.addEventListener(`keyup`, function () {
+      runSearchBar(searchBar.value);
+    });
+  }
+}
+
+function runSearchBar(searchText) {
+  const allEpisodes = getAllEpisodes();
+
+  const epList = allEpisodes
+    .filter((el) => el.summary != null)
+    .filter(
+      (ep) =>
+        ep.name.toLowerCase().includes(searchText.toLowerCase()) ||
+        ep.summary.toLowerCase().includes(searchText.toLowerCase())
+    );
+  filterPageForEpisodes(epList);
+}
+
+function filterPageForEpisodes(episodeList) {
+  let episodesDiv = document.getElementById(`Episods`);
+  episodesDiv.innerHTML = ``;
+  for (let episode of episodeList) {
+    let episodeDiv = document.createElement(`div`);
+    episodeDiv.className = `episodeDiv`;
+    let episodeName = document.createElement(`h4`);
+    let episodeImage = document.createElement(`img`);
+    episodeImage.className = `episodeImage`;
+    let episodeDescribe = document.createElement(`div`);
+    let episodeNumber =
+      (`0` + episode.season).slice(-2) + `E` + (`0` + episode.number).slice(-2);
+    episodeName.innerText = `${episode.name} - S${episodeNumber}`;
+    episodeDescribe.innerHTML = episode.summary;
+    episodesDiv.appendChild(episodeDiv);
+    episodeDiv.appendChild(episodeName);
+    episodeDiv.appendChild(episodeImage);
+    episodeDiv.appendChild(episodeDescribe);
+    try {
+      episodeImage.src = episode.image.medium;
+    } catch {
+      episodeImage.src = "img/notFound.jpg";
+    }
   }
 }
 
